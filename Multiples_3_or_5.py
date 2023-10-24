d098e9f2e701aa03f9dc25dfcc0bd3d8739ffd20
baurdiy <baurzhanerdaliev@gmail.com>
2023-10-23 20:36:44 -0400

    Project Euler, problem number 1

---
+++ Multiples_3_or_5.py
@@ -0,0 +1,23 @@
+"""
+Project Euler Problem 1: https://projecteuler.net/problem=1
+
+Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
+def multiples_3_5(n):
+    
+    l = []
+    for i in range(1, n):
+        if i % 3 == 0 or i % 5 == 0:
+            l.append(i)
+
+    return sum(l)
+
+print(multiples_3_5(10))
+
