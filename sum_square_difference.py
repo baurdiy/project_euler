c0b64b2bd104ce11581c5f70367e3786a5f33bf2
Bauyrzhan Yerdaliyev <baurzhanerdaliev@gmail.com>
2023-11-23 20:18:34 -0500

    problem 6 solved using lists

--- sum_square_difference.py
+++ sum_square_difference.py
@@ -15,3 +15,29 @@ natural numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
 """
+
+# square_sum = []
+# natural_numbers = []
+# for i in range(1, 11):
+#     square_sum.append(i ** 2)
+#     natural_numbers.append(i)
+    
+
+# print(sum(square_sum))
+# print(sum(natural_numbers) ** 2)
+# print((sum(natural_numbers) ** 2) - sum(square_sum))
+
+def sum_square_difference(n: int = 100) -> int:
+    """ Difference between sum of the squares and square of the sum from 1 to n """
+    square_of_sum = []
+    sum_of_squares = []
+    for i in range(1, n + 1):
+        square_of_sum.append(i)
+        sum_of_squares.append(i ** 2)
+    
+    return sum(square_of_sum) ** 2 - sum(sum_of_squares)
+    
+
+if __name__ == "__main__":
+    print(f"{sum_square_difference(20) = }")
+
